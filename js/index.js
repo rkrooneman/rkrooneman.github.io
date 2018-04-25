42f2ef39f4e36cf63333e853880379e845d0f20b
rkrooneman <38614585+rkrooneman@users.noreply.github.com>
2018-04-25 19:23:42 +0200

    Big Change

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+var navBar = $("#topNav");
+var hdrHeight = $("header").height();
+
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdrHeight + 50) {
+    navBar.addClass("navScrolled");
+  } else {
+    navBar.removeClass("navScrolled");
+  }
+});
+
 $l = $('.left');
 $r = $('.right');
 
