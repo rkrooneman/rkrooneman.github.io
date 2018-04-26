74ea68f9f0e1e1fd42d226b82529a0c31733f274
rkrooneman <38614585+rkrooneman@users.noreply.github.com>
2018-04-26 07:39:27 +0200

    Revert

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-var navBar = $("#topNav");
-var hdrHeight = $("header").height();
-
-
-$(window).scroll(function() {
-  if( $(this).scrollTop() > hdrHeight + 50) {
-    navBar.addClass("navScrolled");
-  } else {
-    navBar.removeClass("navScrolled");
-  }
-});
-
 $l = $('.left');
 $r = $('.right');
 
