20e031c60c4b57362dc13ebe4dd7ee3fd6026f0d
rkrooneman <38614585+rkrooneman@users.noreply.github.com>
2018-05-01 20:16:24 +0200

    no carousel

--- js/index.js
+++ js/index.js
@@ -24,26 +24,3 @@ $r.mouseenter(function() {
 }).mouseleave(function() {
   $('.container').removeClass('right-is-hovered');
 });
-
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  navText: [
-    "<i class='fa fa-caret-left'></i>",
-    "<i class='fa fa-caret-right'></i>"
-  ],
-  autoplay: true,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 3
-    },
-    1000: {
-      items: 5
-    }
-  }
-})
