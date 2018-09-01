14f9fa1b350215d1f2ea50f05da8e8982beb63ab
rkrooneman <38614585+rkrooneman@users.noreply.github.com>
2018-09-01 15:21:00 +0200

    backtotopbuttontest

--- js/index.js
+++ js/index.js
@@ -24,3 +24,24 @@ $r.mouseenter(function() {
 }).mouseleave(function() {
   $('.container').removeClass('right-is-hovered');
 });
+
+var amountScrolled = 200;
+var amountScrolledNav = 25;
+
+$(window).scroll(function() {
+  if ( $(window).scrollTop() > amountScrolled ) {
+    $('button.back-to-top').addClass('show');
+  } else {
+    $('button.back-to-top').removeClass('show');
+  }
+});
+
+$('button.back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 800);
+  return false;
+});
+
+Waves.attach('button.back-to-top', 'waves-effect');
+Waves.init();
