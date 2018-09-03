f0546d66cdd7b583ab5fd2631b57fd09836c060c
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2018-09-03 13:54:41 +0200

    345454

--- js/index.js
+++ js/index.js
@@ -25,13 +25,3 @@ $r.mouseenter(function() {
   $('.container').removeClass('right-is-hovered');
 });
 
-var amountScrolled = 200;
-var amountScrolledNav = 25;
-
-$(window).scroll(function() {
-  if ( $(window).scrollTop() > amountScrolled ) {
-    $('button.back-to-top').addClass('show');
-  } else {
-    $('button.back-to-top').removeClass('show');
-  }
-});
