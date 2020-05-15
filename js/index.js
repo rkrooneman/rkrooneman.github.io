4764d0e4b30ee8a0a7f979d90af2891f8f7f0de9
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-15 19:31:28 +0200

    ewfwef

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
-$(document).ready(function() {
-        function dateTime() {
-            var format = "";
-            var ndate = new Date();
-            var hr = ndate.getHours();
-            var h = hr % 12;
+        $(document).ready(function() {
+            function dateTime() {
+                var format = "";
+                var ndate = new Date();
+                var hr = ndate.getHours();
+                var h = hr % 12;
 
-            if (hr < 12) {
-                greet = 'Good morning...';
-                format = 'AM';
-            } else if (hr >= 12 && hr <= 17) {
-                greet = 'Good afternoon...';
-                format = 'PM';
-            } else if (hr >= 17 && hr <= 24)
-                greet = 'Good evening...';
-        }
+                if (hr < 12) {
+                    greet = 'Good morning...';
+                    format = 'AM';
+                } else if (hr >= 12 && hr <= 17) {
+                    greet = 'Good afternoon...';
+                    format = 'PM';
+                } else if (hr >= 17 && hr <= 24)
+                    greet = 'Good evening...';
+            }
 
-        setInterval(dateTime, 1000);
-    });
+            setInterval(dateTime, 1000);
+            
+            });
