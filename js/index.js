652a2660c2dbb73b0adaf8ff5ce40d6a5be076a1
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-15 19:36:10 +0200

    wefwef

--- js/index.js
+++ js/index.js
@@ -1,20 +1,42 @@
-        $(document).ready(function() {
-            function dateTime() {
-                var format = "";
-                var ndate = new Date();
-                var hr = ndate.getHours();
-                var h = hr % 12;
+$(document).ready(function () {
+    function dateTime() {
+        var format = "";
+        var ndate = new Date();
+        var hr = ndate.getHours();
+        var h = hr % 12;
 
-                if (hr < 12) {
-                    greet = 'Good morning...';
-                    format = 'AM';
-                } else if (hr >= 12 && hr <= 17) {
-                    greet = 'Good afternoon...';
-                    format = 'PM';
-                } else if (hr >= 17 && hr <= 24)
-                    greet = 'Good evening...';
-            }
+        if (hr < 12) {
+            greet = 'Good morning...';
+            format = 'AM';
+        } else if (hr >= 12 && hr <= 17) {
+            greet = 'Good afternoon...';
+            format = 'PM';
+        } else if (hr >= 17 && hr <= 24)
+            greet = 'Good evening...';
+    }
 
-            setInterval(dateTime, 1000);
-            
-            });
+    setInterval(dateTime, 1000);
+
+var app = document.getElementById('app');
+var typewriter = new Typewriter(app, {
+    loop: false,
+    delay: 75,
+});
+
+typewriter.typeString('Hello there...')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString(greet)
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Please come closer...')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('or scroll down...')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Coffee?')
+    .pauseFor(1500)
+    .start();
+    
+});
