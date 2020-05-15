0ea370c675725eba4966617c95edd4bf0f498466
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-15 20:03:05 +0200

    wdqwdwqd

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     function dateTime() {
-        var format = "";
-        var ndate = new Date();
         var hr = ndate.getHours();
         var h = hr % 12;
 
@@ -14,27 +12,15 @@ $(document).ready(function () {
         } else if (hr >= 17 && hr <= 24)
             greet = 'Good evening...';
 
-        var m = ndate.getMinutes().toString();
-        var s = ndate.getSeconds().toString();
-
         if (h < 12) {
             h = "0" + h;
-            $("h2.day__greet").html(greet);
+            $("strong.day__greet").html(greet);
         } else if (h < 18) {
-            $("h2.day__greet").html(greet);
+            $("strong.day__greet").html(greet);
         } else {
-            $("h2.day__greet").html(greet);
+            $("strong.day__greet").html(greet);
         }
 
-        if (s < 10) {
-            s = "0" + s;
-        }
-
-        if (m < 10) {
-            m = "0" + m;
-        }
-
-        $('.date').html(h + ":" + m + ":" + s + format);
     }
 
     setInterval(dateTime, 1000); 
@@ -49,7 +35,7 @@ var typewriter = new Typewriter(app, {
 typewriter.typeString('Hello there...')
     .pauseFor(1500)
     .deleteAll()
-    .typeString(greet)
+    .typeString('Good <strong class="day__greet></strong>"')
     .pauseFor(1500)
     .deleteAll()
     .typeString('please come closer...')
