be813c2c2d0c34a01e74559dd65730bc7f57b00a
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-15 18:52:57 +0200

    Typewriter.js

--- js/index.js
+++ js/index.js
@@ -1,30 +1,7 @@
-{
-var nav = $("nav");
-var hdrHeight = $("header").height();
-
-
-$(window).scroll(function() {
-  if( $(this).scrollTop() > hdrHeight + 50) {
-    nav.addClass("navScrolled");
-  } else {
-    nav.removeClass("navScrolled");
-  }
-});
-}
-
-{
-$l = $('.left');
-$r = $('.right');
-
-$l.mouseenter(function() {
-  $('.container').addClass('left-is-hovered');
-}).mouseleave(function() {
-  $('.container').removeClass('left-is-hovered');
+var   app = document.getElementById('app');
+var   typewriter = new Typewriter(app, {
+        loop: false,
+        delay: 75,
 });
 
-$r.mouseenter(function() {
-  $('.container').addClass('right-is-hovered');
-}).mouseleave(function() {
-  $('.container').removeClass('right-is-hovered');
-});
-}
+typewriter.typeString('Hello there...');
