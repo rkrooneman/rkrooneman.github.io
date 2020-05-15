1ded9ad1b1ee00e3dfd920023a6610490241f4e9
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-15 18:56:59 +0200

    Typewriter expanded

--- js/index.js
+++ js/index.js
@@ -4,4 +4,18 @@ var   typewriter = new Typewriter(app, {
         delay: 75,
 });
 
-typewriter.typeString('Hello there...');
+typewriter.typeString('Hello there...')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('How are you doing?')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Please come closer...')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('or scroll down...')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Coffee?')
+    .pauseFor(1500)
+    .start();
