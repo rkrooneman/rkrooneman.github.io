a9b838c75c6cdd2b72c562a691e1a58bdc719f5e
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-15 20:10:21 +0200

    wefwefwe

--- js/index.js
+++ js/index.js
@@ -5,20 +5,18 @@ $(document).ready(function () {
 
         if (hr < 12) {
             greet = 'Good morning...';
-            format = 'AM';
         } else if (hr >= 12 && hr <= 17) {
             greet = 'Good afternoon...';
-            format = 'PM';
         } else if (hr >= 17 && hr <= 24)
             greet = 'Good evening...';
 
         if (h < 12) {
             h = "0" + h;
-            $("strong.day__greet").html(greet);
+            $(".day__greet").html(greet);
         } else if (h < 18) {
-            $("strong.day__greet").html(greet);
+            $(".day__greet").html(greet);
         } else {
-            $("strong.day__greet").html(greet);
+            $(".day__greet").html(greet);
         }
 
     }
