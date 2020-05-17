95253bcd52f7486bd6f68276a4224e891eab9405
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-17 08:41:43 +0200

    qwdwqd

--- js/index.js
+++ js/index.js
@@ -82,7 +82,7 @@ $(document).ready(function () {
         console.log('menu1', menu_pos1);
         console.log('scroll', scroll);
 
-        if (scroll > (dark_pos - svg_height) && menu_pos1 < (dark_pos + dark_height + svg_height)) {
+        if (scroll > (dark_pos - svg_height) && menu_pos1 < (dark_pos + dark_height + 100)) {
             $('.top').addClass('nav__white');
             $('.top').removeClass('nav__black');
         } else if (scroll > (dark_pos2 - svg_height) && menu_pos1 < (dark_pos2 + dark_height2 + svg_height)) {
@@ -109,7 +109,7 @@ $(document).ready(function () {
 
         console.log('menu2', menu_pos2);
 
-        if (scroll > (dark_pos - 100) && menu_pos2 < (dark_pos + dark_height + svg_height)) {
+        if (scroll > (dark_pos - 100) && menu_pos2 < (dark_pos + dark_height + 100)) {
             $('.bottom').addClass('nav__white');
             $('.bottom').removeClass('nav__black');
         } else if (scroll > (dark_pos2 - svg_height) && menu_pos2 < (dark_pos2 + dark_height2 + svg_height)) {
