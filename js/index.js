18b1f15574fbf5e2539fe7242e707a6cee3fef87
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-17 19:37:25 +0200

    qwdwqdq

--- js/index.js
+++ js/index.js
@@ -15,25 +15,19 @@ $(document).ready(function () {
     '🎧',
     ];
 
-        var random__interest1 = Math.floor(Math.random() * textArray.length);
-        var random__interest2 = Math.floor(Math.random() * textArray.length);
-        var random__interest3 = Math.floor(Math.random() * textArray.length);
+    var random__interest1 = Math.floor(Math.random() * textArray.length);
+    var random__interest2 = Math.floor(Math.random() * textArray.length);
+    var random__interest3 = Math.floor(Math.random() * textArray.length);
+        
+    console.log('interest1', textArray[random__interest1]);
+    console.log('interest2', textArray[random__interest2]);
+    console.log('interest3', textArray[random__interest3]);
+    
+    $('.interest1').text(textArray[random__interest1]);
+    $('.interest2').text(textArray[random__interest2]);
+    $('.interest3').text(textArray[random__interest3]);
 
-        console.log('interest1', textArray[random__interest1]);
-        console.log('interest2', textArray[random__interest2]);
-        console.log('interest3', textArray[random__interest3]);
-
-        $('.interest1').ready(function () {
-            $(this).text(textArray[random__interest1]);
-        });
-        $('.interest2').ready(function () {
-            $(this).text(textArray[random__interest2]);
-        });
-        $('.interest3').ready(function () {
-            $(this).text(textArray[random__interest3]);
-        });
-
-    }
+    
 
     setInterval(randominterest, 1500);
 });
