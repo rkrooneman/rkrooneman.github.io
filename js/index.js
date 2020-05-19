62c46e14c01cedd5d0cd7dd286a912ce72c5cc30
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-19 15:38:06 +0200

    wefewf

--- js/index.js
+++ js/index.js
@@ -203,20 +203,20 @@ $(document).ready(function () {
     });
     
     $("#exp__b").click(function () {
-        $("#exp__1").show();
+        $("#exp__1").hide();
         $("#exp__1").css("opacity", "0");
-        $("#exp__2").hide();
+        $("#exp__2").show();
         $("#exp__2").css("opacity", "1");
         $("#exp__3").hide();
         $("#exp__3").css("opacity", "0");
     });
     
     $("#exp__c").click(function () {
-        $("#exp__1").show();
+        $("#exp__1").hide();
         $("#exp__1").css("opacity", "0");
         $("#exp__2").hide();
         $("#exp__2").css("opacity", "0");
-        $("#exp__3").hide();
+        $("#exp__3").show();
         $("#exp__3").css("opacity", "1");
     });  
 
