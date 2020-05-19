966590ef0cb3b29872b4407d983db6339d871a13
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2020-05-19 15:54:16 +0200

    qwdwqd

--- js/index.js
+++ js/index.js
@@ -197,9 +197,9 @@ $(document).ready(function () {
         $("#exp__1").show();
         $("#exp__1").fadeTo("slow", 1);
         $("#exp__2").hide();
-        $("#exp__1").css("opacity", "0");  
+        $("#exp__2").css("opacity", "0");  
         $("#exp__3").hide();
-        $("#exp__1").css("opacity", "0");  
+        $("#exp__3").css("opacity", "0");  
     });
     
     $("#exp__b").click(function () {
@@ -208,14 +208,14 @@ $(document).ready(function () {
         $("#exp__2").show();
         $("#exp__2").fadeTo("slow", 1);
         $("#exp__3").hide();
-        $("#exp__1").css("opacity", "0");  
+        $("#exp__3").css("opacity", "0");  
     });
     
     $("#exp__c").click(function () {
         $("#exp__1").hide();
         $("#exp__1").css("opacity", "0");  
         $("#exp__2").hide();
-        $("#exp__1").css("opacity", "0");  
+        $("#exp__2").css("opacity", "0");  
         $("#exp__3").show();
         $("#exp__3").fadeTo("slow", 1);
     });  
